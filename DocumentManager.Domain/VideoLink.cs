46fcfa9d51affb07c1dfe5998fb2e588cb2c0192
dragonslayp@mail.ru <dragonslayp@mail.ru>
2022-05-08 22:30:22 +0300

    delete unsed string

--- DocumentManager.Domain/VideoLink.cs
+++ DocumentManager.Domain/VideoLink.cs
@@ -5,7 +5,6 @@ namespace DocumentManager.Domain
     public class VideoLink
     {
         public Guid Id { get; set; }
-        //public string ServiceUrl { get; set; }
         public Guid VideoId { get; set; }
         public Document Document { get; set; }
         public VideoLink(Guid videoId)
